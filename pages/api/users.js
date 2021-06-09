2ecce091fea61002e67d38d717350139f11c78a5
Elom Gomez <no-itsbackpack@github.com>
2021-06-09 14:03:17 -0500

    we ready

---
+++ pages/api/users.js
@@ -0,0 +1,29 @@
+// Next.js API route support: https://nextjs.org/docs/api-routes/introduction
+import PSDB from 'planetscale-node'
+
+export default async (req, res) => {
+  const {
+    body: { email, name, password },
+    method
+  } = req
+  const conn = new PSDB('main')
+
+  switch (method) {
+    case 'POST':
+      const [rows, fields] = await conn.query(
+        `insert into users (email, name, password) values ('${email}', '${name}', '${password}')`
+      )
+      res.statusCode = 201
+      res.json({})
+      break
+    case 'GET':
+      const [getRows, _] = await conn.query('select * from users')
+
+      res.statusCode = 200
+      res.json(getRows)
+      break
+    default:
+      res.setHeader('Allow', ['GET', 'PUT'])
+      res.status(405).end(`Method ${method} Not Allowed`)
+  }
+}
--- pages/index.js
+++ pages/index.js
@@ -1,65 +1,38 @@
-import Head from 'next/head'
-import styles from '../styles/Home.module.css'
+import useSWR from 'swr'
+
+const fetcher = (url) => fetch(url).then((res) => res.json())
 
 export default function Home() {
+  const { data, error } = useSWR('/api/users', fetcher)
+
+  if (error) return <div>Failed to load</div>
+  if (!data) return <div>Loading...</div>
+
+  const users = []
+
   return (
-    <div className={styles.container}>
-      <Head>
-        <title>Create Next App</title>
-        <link rel="icon" href="/favicon.ico" />
-      </Head>
-
-      <main className={styles.main}>
-        <h1 className={styles.title}>
-          Welcome to <a href="https://nextjs.org">Next.js!</a>
-        </h1>
-
-        <p className={styles.description}>
-          Get started by editing{' '}
-          <code className={styles.code}>pages/index.js</code>
-        </p>
-
-        <div className={styles.grid}>
-          <a href="https://nextjs.org/docs" className={styles.card}>
-            <h3>Documentation &rarr;</h3>
-            <p>Find in-depth information about Next.js features and API.</p>
-          </a>
-
-          <a href="https://nextjs.org/learn" className={styles.card}>
-            <h3>Learn &rarr;</h3>
-            <p>Learn about Next.js in an interactive course with quizzes!</p>
-          </a>
-
-          <a
-            href="https://github.com/vercel/next.js/tree/master/examples"
-            className={styles.card}
-          >
-            <h3>Examples &rarr;</h3>
-            <p>Discover and deploy boilerplate example Next.js projects.</p>
-          </a>
-
-          <a
-            href="https://vercel.com/import?filter=next.js&utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className={styles.card}
-          >
-            <h3>Deploy &rarr;</h3>
-            <p>
-              Instantly deploy your Next.js site to a public URL with Vercel.
-            </p>
-          </a>
-        </div>
-      </main>
-
-      <footer className={styles.footer}>
-        <a
-          href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Powered by{' '}
-          <img src="/vercel.svg" alt="Vercel Logo" className={styles.logo} />
-        </a>
-      </footer>
+    <div className='min-h-screen flex  bg-white py-12 px-4 sm:px-6 lg:px-8'>
+      <div className='max-w-md w-full space-y-8'>
+        <table className='border-separate border border-green-800 table-fixed'>
+          <thead>
+            <tr>
+              <th className='w-1/2 p-2'>Name</th>
+              <th className='w-1/2 p-2'>Email</th>
+            </tr>
+          </thead>
+          <tbody>
+            {data.map((user) => (
+              <tr key={user.id}>
+                <td className='border border-green-600 p-2'>{user.email}</td>
+                <td className='border border-green-600 p-2'>{user.name}</td>
+                <td className='border border-green-600 p-2'>
+                  <button className='text-white bg-red-600 hover:bg-red-700 border-rounded p-2'>Remove</button>
+                </td>
+              </tr>
+            ))}
+          </tbody>
+        </table>
+      </div>
     </div>
   )
 }
